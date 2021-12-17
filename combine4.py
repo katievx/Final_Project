1802e677bf7483dea5962e47afd0a09ae1d6721a
opeyemif <64177574+opeyemif@users.noreply.github.com>
2021-12-16 21:49:13 -0500

    Add files via upload

--- combine4.py
+++ combine4.py
@@ -1,6 +1,5 @@
 from argparse import ArgumentParser
 import sys
-from numpy import NaN
 import pandas as pd
 import matplotlib.pyplot as plt
 from pandas.io.parsers import read_csv 
@@ -32,22 +31,19 @@ class Recipe:
         Side effects:
             Modifies the dataframe by adding new column 
         """
-    #    for line in self.cookies_and_cakes_df:
-    #        if self.cookies_and_cakes_df["Serving Size"] == NaN:
-     #           print(f"Please replace {line} with a non-zero or non-empty value!")
-      #          break
-       #     else:
         self.cookies_and_cakes_df["Calories per cookie or slice"] =  self.cookies_and_cakes_df["Calories"] / self.cookies_and_cakes_df["Serving Size"]
+        print("Here is the current dataframe of the two csv files:")
+        print(self.cookies_and_cakes_df)
         
-    def sort(self, cookies_and_cakes_df):
+    def sort(self):
         """Orders the dataframe by different categories, including:
             calories, bake time, and serving size
         Returns:
             Three ranks of the dataframe categories
         """
-        calorie_rank = cookies_and_cakes_df.sort_values(["Calories"])
-        baketime_rank = cookies_and_cakes_df.sort_values(["Bake Time (minutes)"])
-        servingsize_rank = cookies_and_cakes_df.sort_values(["Serving Size"])
+        calorie_rank = self.cookies_and_cakes_df.sort_values(["Calories"])
+        baketime_rank = self.cookies_and_cakes_df.sort_values(["Bake Time (minutes)"])
+        servingsize_rank = self.cookies_and_cakes_df.sort_values(["Serving Size"])
         return (calorie_rank, baketime_rank, servingsize_rank)
         
     def bars(self):
@@ -55,19 +51,19 @@ class Recipe:
         Returns:
             Three bar graphs of categories from the recipe dataframe
         """
-        self.calories_bar = self.cookies_and_cakes_df.plot.bar(x = 'Recipe Name', y = 'Calories')
+        calories_bar = self.cookies_and_cakes_df.plot.bar(x = 'Recipe Name', y = 'Calories')
         servingsize_bar = self.cookies_and_cakes_df.plot.bar(x = 'Recipe Name', y = 'Serving Size')
         baketime_bar = self.cookies_and_cakes_df.plot.bar(x = 'Recipe Name', y = 'Bake Time (minutes)')
-        return (self.calories_bar, self.servingsize_bar, self.baketime_bar)
+        return (calories_bar, servingsize_bar, baketime_bar)
 
-    def hists(self, cookies_and_cakes_df):
+    def hists(self):
         """Create histograms from the dataframe
         Returns:
             Three histograms of categories from the recipe dataframe
         """
-        calories_g = cookies_and_cakes_df.hist("Calories")
-        servingsize_g = cookies_and_cakes_df.hist("Serving size")
-        baketime_g = cookies_and_cakes_df.hist("Bake Time (minutes)")
+        calories_g = self.cookies_and_cakes_df.hist("Calories")
+        servingsize_g = self.cookies_and_cakes_df.hist("Serving size")
+        baketime_g = self.cookies_and_cakes_df.hist("Bake Time (minutes)")
         return (calories_g, servingsize_g, baketime_g)
 
     def user_input(self):
@@ -82,6 +78,7 @@ class Recipe:
         user_1 = input("Which option would you like: a list or graph?").lower()
         return user_1
 
+
 def list_options(recipes):
     """If user chose the list visualization, ask what they would want to sort by and print
         their visualization
@@ -90,10 +87,9 @@ def list_options(recipes):
     Side effects:
         Print rank that is inputted by user
     """
-    calorie_rank, baketime_rank, servingsize_rank = recipes.sort()
-  #  if user_1 == "list":
+    calorie_rank, baketime_rank, servingsize_rank = recipes
+    user_2 = input("What would you like the dataframe listed by: calories, bake time, or serving size?").lower()
     while True:
-        user_2 = input("What would you like the dataframe listed by: calories, bake time, or serving size?").lower()
         if user_2 == "calories":
             print(calorie_rank)
             break
@@ -106,44 +102,48 @@ def list_options(recipes):
         else:
             print("We don't have that option! Please input either 'calories', 'serving size', or 'bake time' ")  
 
-def bar_options(cookies_and_cakes_df):
+def bar_options(recipes):
     """If user chose the bar graph visualization, ask what they would want to sort by and print
         their visualization as a bar graph
     Side effects:
         Print bar graph that is inputted by user
     """
- #   if user_1 == "graph":
- #       user_2 = input("Do you want to see a histogram or bar graph?").lower()
+    calories_bar, servingsize_bar, baketime_bar = recipes
+    
+    user_3 = input("What would you like the bar graph listed by: calories, bake time, or serving size?").lower()
     while True:
- #       if user_2 == "bar graph":
-        user_3 = input("What would you like the bar graph listed by: calories, bake time, or serving size?").lower()
         if user_3 == "calories":
-            print(calories_bar)
+            plt.show(calories_bar)
             break
         if user_3 == "bake time":
-            print(baketime_bar)
+            plt.show(baketime_bar)
             break
         if user_3 == "serving size":
-            print(servingsize_bar)
+            plt.show(servingsize_bar)
             break
         else:
             print("We don't have that option! Please input either 'calories', 'serving size', or 'bake time' ")
- #   return user_2
+
                                     
-def hist_options(cookies_and_cakes_df):
+def hist_options(recipes):
     """If user chose the histogram visualization, ask what they would want to sort by and print
         their visualization as a histogram
     Side effects:
         Print histogram that is inputted by user
     """
-    #    if user_2 == "histogram":
-        user_3 = "What would you like a histogram for: calories, serving size, or bake time?".lower()
+    calories_g, servingsize_g, baketime_g = recipes
+    
+    user_3 = "What would you like a histogram for: calories, serving size, or bake time?".lower()
+    while True:
         if user_3 == "calories":
-            print(calories_g)
+            plt.show(calories_g)
+            break
         if user_3 == "serving size":
-            print(servingsize_g)
+            plt.show(servingsize_g)
+            break
         if user_3 == "bake time":
-            print(baketime_g)
+            plt.show(baketime_g)
+            break
         else:
             print("We don't have that option! Please input either 'calories', 'serving size', or 'bake time' ")
 
@@ -152,15 +152,22 @@ def user_choice(user_1):
     Returns:
         The visualization chosen by the user
     """
+    while True:
         if user_1 == "list":
-            list_options(cookies_and_cakes_df)
+            list_options
+            break
         if user_1 == "graph":
-            user_2 = input("Do you want to see a histogpython2ram or bar graph?").lower()
-            if user_2 == "bar graph":
-                bar_options(cookies_and_cakes_df)
-            if user_2 == "histogram":
-                hist_options(cookies_and_cakes_df)
-        return user_2
+            user_n = input("Do you want to see a histogram or bar graph?").lower()
+            if user_n == "bar graph":
+                bar_options
+                break
+            if user_n == "histogram":
+                hist_options
+                break
+        else:
+            print("We don't have that option! Please input either 'list', 'graph' ")
+            
+            return user_n
                 
                 
 def main(file1, file2):   
@@ -175,13 +182,21 @@ def main(file1, file2):
     user_c = recipes.user_input()
     user_choice(user_c)
     
-    #for displaying based on input
-    cal_df = recipes.cal_per_serving_size()
-    hist_options(cal_df)
-    bar_options(cal_df)
-    list_options(cal_df)    
+    if user_c == "list":
+        list_op = recipes.sort()
+        list_options(list_op)
+        
+    user_g = user_choice
     
+    if user_g == "bar graph":
+        bar_op = recipes.bars()
+        bar_options(bar_op)
+    if user_g == "histogram":
+        hist_op = recipes.hists()
+        hist_options(hist_op)
 
+   
+    
 def parse_args(arglist):
     parser = ArgumentParser()
     parser.add_argument("file1",
